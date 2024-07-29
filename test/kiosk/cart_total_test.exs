412b7547b38710fa385cfedbb50501221bf2b64d
Dennis Fallin <dfalling@gmail.com>
2024-07-29 22:24:29 +0300

    Tidy up tests

--- test/kiosk/cart_total_test.exs
+++ test/kiosk/cart_total_test.exs
@@ -25,15 +25,11 @@ defmodule Kiosk.CartTotalTest do
 
   describe "calculate" do
     test "calculate/1 returns total for items with no offers" do
-      cart_items = [@green_tea, @strawberries]
-
-      assert CartTotal.calculate(cart_items) == 8.11
+      assert CartTotal.calculate([@green_tea, @strawberries]) == 8.11
     end
 
     test "calculate/1 returns total for coffee with no offers" do
-      cart_items = [@coffee, @coffee]
-
-      assert CartTotal.calculate(cart_items) == 22.46
+      assert CartTotal.calculate([@coffee, @coffee]) == 22.46
     end
 
     test "calculate/1 returns buy-one-get-one-free deal for green tea" do
@@ -43,41 +39,19 @@ defmodule Kiosk.CartTotalTest do
     end
 
     test "calculate/1 returns normal price for two strawberries" do
-      cart_items = [
-        @strawberries,
-        @strawberries
-      ]
-
-      assert CartTotal.calculate(cart_items) == 10.00
+      assert CartTotal.calculate([@strawberries, @strawberries]) == 10.00
     end
 
     test "calculate/1 returns fixed 4.50 deal price for three or more strawberries" do
-      cart_items = [
-        @strawberries,
-        @strawberries,
-        @strawberries
-      ]
-
-      assert CartTotal.calculate(cart_items) == 13.50
+      assert CartTotal.calculate([@strawberries, @strawberries, @strawberries]) == 13.50
     end
 
-    test "calculate/1 returns full price for less than 3 coffee" do
-      cart_items = [
-        @coffee,
-        @coffee
-      ]
-
-      assert CartTotal.calculate(cart_items) == 22.46
+    test "calculate/1 returns full price for less than three coffee" do
+      assert CartTotal.calculate([@coffee, @coffee]) == 22.46
     end
 
     test "calculate/1 returns 2/3 price for three or more coffee" do
-      cart_items = [
-        @coffee,
-        @coffee,
-        @coffee
-      ]
-
-      assert CartTotal.calculate(cart_items) == 33.69 * 2 / 3
+      assert CartTotal.calculate([@coffee, @coffee, @coffee]) == 33.69 * 2 / 3
     end
   end
 end
