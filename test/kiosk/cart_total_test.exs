93fde4edc5bf59c537012a8d6332aa76b7c542ea
Dennis Fallin <dfalling@gmail.com>
2024-07-29 21:55:55 +0300

    Create product fixtures

--- test/kiosk/cart_total_test.exs
+++ test/kiosk/cart_total_test.exs
@@ -3,39 +3,35 @@ defmodule Kiosk.CartTotalTest do
 
   alias Kiosk.CartTotal
 
-  describe "calculate" do
-    alias Kiosk.Inventory.Product
+  alias Kiosk.Inventory.Product
 
+  @green_tea %Product{
+    code: "GR1",
+    name: "Green Tea",
+    price: 3.11
+  }
+
+  @strawberries %Product{
+    code: "SR1",
+    name: "Strawberries",
+    price: 5.00
+  }
+
+  @coffee %Product{
+    code: "CF1",
+    name: "Coffee",
+    price: 11.23
+  }
+
+  describe "calculate" do
     test "calculate/1 returns total for items with no offers" do
-      cart_items = [
-        %Product{
-          code: "GR1",
-          name: "Green Tea",
-          price: 3.11
-        },
-        %Product{
-          code: "SR1",
-          name: "Strawberries",
-          price: 5.00
-        }
-      ]
+      cart_items = [@green_tea, @strawberries]
 
       assert CartTotal.calculate(cart_items) == 8.11
     end
 
     test "calculate/1 returns total for duplicate items with no offers" do
-      cart_items = [
-        %Product{
-          code: "CF1",
-          name: "Coffee",
-          price: 11.23
-        },
-        %Product{
-          code: "CF1",
-          name: "Coffee",
-          price: 11.23
-        }
-      ]
+      cart_items = [@coffee, @coffee]
 
       assert CartTotal.calculate(cart_items) == 22.46
     end
